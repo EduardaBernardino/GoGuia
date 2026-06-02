c1e1e4ec5c1f8349c96cc088b88a276e35705b84
EDUARDA DE OLIVEIRA BERNARDINO <eduarda.bernardino@a.ucb.br>
2026-06-02 20:57:23 -0300

    fix: Adaptação no import do nome do banco

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: './src/drizzle/db/schema.ts',
   dialect: 'sqlite',
   dbCredentials: {
-    url: `file:./db_goguia.db`,
+    url: `file:./${process.env.DB_FILE_NAME}.db`,
   },
 });
