779a5d819f0a16f16db3a7ec4e980fdfefbc58dd
Eduarda Bernardino <eduardabernardinoo06@gmail.com>
2026-06-12 20:20:26 -0300

    feat: adiciona telas de carteira do guia e do usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,75 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import Login from './pages/Login';
+
 import EsqueciSenha from './pages/EsqueciSenha';
+
 import ConfirmacaoEmail from './pages/Confirmacaoemail';
+
 import Home from './pages/home';
+
 import TelaAvaliacao from './pages/TelaAvaliacao';
+
 import Agendamentos from './pages/Agendamentos';
+
 import Pagamento from './pages/Pagamento';
+
 import MeusRoteiros from './pages/MeusRoteiros';
+
 import Favoritos from './pages/Favoritos';
+
 import Configuracoes from './pages/Configuracoes';
 
+
+
+
+
 function App() {
+
   return (
-    <BrowserRouter>
+
+     <BrowserRouter>
+
       <Routes>
+
         <Route path="/" element={<Navigate to="/login" replace />} />
+
         <Route path="/login" element={<Login />} />
+
         <Route path="/esqueci-senha" element={<EsqueciSenha />} />
+
         <Route path="/confirmacao-email" element={<ConfirmacaoEmail />} />
 
+
+
         <Route path="/home" element={<Home />} />
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/main
-        {/* <Route path="/home/tela-avaliacao" element={<TelaAvaliacao />} />
-        <Route path="/home/tela-avaliacao/detalhes" element={<Agendamentos />} />
-        <Route path="/home/pagamento" element={<Pagamento />} /> */}
+ {/* <Route path="/home/tela-avaliacao" element={<TelaAvaliacao />} />
 
-<<<<<<< HEAD
-        <Route path="/home/pagamento" element={<Pagamento />} /> */}
+        <Route path="/home/tela-avaliacao/detalhes" element={<Agendamentos />} />
 
-=======
+        <Route path="/home/pagamento" element={<Pagamento />} /> */}
+
+
+
+       <Route path="/home/pagamento" element={<Pagamento />} />
+
+
+
         <Route path="/meus-roteiros" element={<MeusRoteiros />} />
+
         <Route path="/favoritos" element={<Favoritos />} />
+
         <Route path="/configuracoes" element={<Configuracoes />} />
->>>>>>> origin/main
-      </Routes>
-    </BrowserRouter>
-  );
+
+     </Routes>
+
+ </BrowserRouter>
+
+ );
+
 }
+
+
 
 export default App;
