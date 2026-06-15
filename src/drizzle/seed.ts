e191c99f067454492b8ffe3594c5e2d6c2826f39
Gabriel-Santos-S <gabrielsouto957@gmail.com>
2026-06-15 10:05:25 -0300

    fix: Correção na seed 'tipo pagamento'

--- src/drizzle/seed.ts
+++ src/drizzle/seed.ts
@@ -1,7 +1,10 @@
 import { closeServer, db } from '../core/config/server';
-import { perfil } from './db/schema';
+import { perfil, statusPagamento, statusReserva, statusRoteiro, tipoPagamento } from './db/schema';
 
 async function seed() {
+    //====================
+    // Prefis
+    //====================
     const perfisData = [
         { nome: 'Administrador' },
         { nome: 'Usuário Comum' },
@@ -11,8 +14,63 @@ async function seed() {
     console.log('🌱 Seeding perfil...');
     await db.insert(perfil).values(perfisData);
 
-    console.log('✅ Seed completed!');
+    console.log('✅ Seed perfil completed!\n');
 
+
+    //====================
+    // Status Pagamento
+    //====================
+    const statusPagamentoData = [
+        { nome: 'pendente' },
+        { nome: 'concluido' },
+        { nome: 'cancelado' },
+    ];
+
+    console.log('🌱 Seeding status pagamento...');
+    await db.insert(statusPagamento).values(statusPagamentoData);
+
+    console.log('✅ Seed status pagamento completed!\n');
+
+    //====================
+    // Status Reserva
+    //====================
+    const statusReservaData = [
+        { nome: 'pendente' },
+        { nome: 'marcada' },
+        { nome: 'finalizada' },
+        { nome: 'cancelada' },
+    ];
+
+    console.log('🌱 Seeding status reserva...');
+    await db.insert(statusReserva).values(statusReservaData);
+
+    console.log('✅ Seed status reserva completed!\n');
+
+    //====================
+    // Status Roteiro
+    //====================
+    const statusRoteiroData = [
+        { nome: 'ativo' },
+        { nome: 'inativo' },
+    ];
+
+    console.log('🌱 Seeding status roteiro...');
+    await db.insert(statusRoteiro).values(statusRoteiroData);
+
+    console.log('✅ Seed status roteiro completed!\n');
+
+    //====================
+    // Tipo Pagamento
+    //====================
+    const tipoPagamentoData = [
+        { nome: 'cartao' },
+        { nome: 'pix' },
+    ];
+
+    console.log('🌱 Seeding tipo pagamento...');
+    await db.insert(tipoPagamento).values(tipoPagamentoData);
+
+    console.log('✅ Seed tipo pagamento completed!\n');
 
     closeServer()
 }
