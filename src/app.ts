f590a660a7b539f254bbfe87f929528efc580034
Gabriel-Santos-S <gabrielsouto957@gmail.com>
2026-06-15 23:58:07 -0300

    fix: Correção na politica cors

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,19 @@ import ArquivoRouter from "./infra/routes/arquivo.routes";
 
 
 const app = express();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+
 app.use(express.json());
 
 app.use("/pagamento", authMiddleware, PagamentoRouter);
